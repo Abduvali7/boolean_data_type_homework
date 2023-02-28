66675d6e944ad9304983e53df26d24296cbb7387
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-02-28 11:15:49 +0500

    02

--- bool02.py
+++ bool02.py
@@ -1,5 +1,5 @@
 def main(a):
-    a = 7
+    
     """
     check the following statement "The variable 'a' is equal 7"
     Args:
@@ -8,5 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 7%2==1
-print(main(7))
+    return 7
+print(main(7%2==1))
